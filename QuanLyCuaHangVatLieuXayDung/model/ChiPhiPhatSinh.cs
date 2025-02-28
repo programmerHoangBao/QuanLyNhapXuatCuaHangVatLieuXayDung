7da0e8ec65a399eb49b95619b56495cc339f227d
Bao <universitynhb@gmail.com>
2025-02-28 07:51:52 +0700

    Bô sung model ChiPhiPhatSinh.cs

---
+++ QuanLyCuaHangVatLieuXayDung/model/ChiPhiPhatSinh.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangVatLieuXayDung.model
+{
+    internal class ChiPhiPhatSinh
+    {
+        private int index;
+        private string maChiPhi;
+        private DateTime ngayLap;
+        private string noiDung;
+        private double soTien;
+
+        public ChiPhiPhatSinh()
+        {
+        }
+
+        public ChiPhiPhatSinh(int index, string maChiPhi, DateTime ngayLap, string noiDung, double soTien)
+        {
+            this.Index = index;
+            this.MaChiPhi = maChiPhi;
+            this.NgayLap = ngayLap;
+            this.NoiDung = noiDung;
+            this.SoTien = soTien;
+        }
+
+        public int Index { get => index; set => index = value; }
+        public string MaChiPhi { get => maChiPhi; set => maChiPhi = value; }
+        public DateTime NgayLap { get => ngayLap; set => ngayLap = value; }
+        public string NoiDung { get => noiDung; set => noiDung = value; }
+        public double SoTien { get => soTien; set => soTien = value; }
+    }
+}
