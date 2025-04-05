120abcf8470ed872f892fd957ff111ec61508e34
vubaolongkg <156653049+vubaolongkg@users.noreply.github.com>
2025-04-05 22:04:04 +0700

    commit_PhieuTraHang_temp

---
+++ QuanLyCuaHangVatLieuXayDung/service/impl/IPhieuTraHangService.cs
@@ -0,0 +1,16 @@
+﻿using QuanLyCuaHangVatLieuXayDung.model;
+using System;
+using System.Collections.Generic;
+
+namespace QuanLyCuaHangVatLieuXayDung.service.impl
+{
+    internal interface IPhieuTraHangService
+    {
+        void ThemPhieu(PhieuTraHang phieu);
+        void XoaPhieu(string maPhieu);
+        void CapNhatPhieu(PhieuTraHang phieu);
+        PhieuTraHang TimPhieuTheoMa(string maPhieu);
+        List<PhieuTraHang> LayTatCaPhieu();
+        List<PhieuTraHang> TimPhieuTheoNgay(DateTime tuNgay, DateTime denNgay);
+    }
+}
