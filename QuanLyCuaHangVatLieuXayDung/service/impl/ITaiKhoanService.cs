4614b7d84eede2e1b175bc7ea85441ca2bef5fca
kimchunnn <tranthikimchung214@gmail.com>
2025-04-06 16:53:15 +0700

    code impl và service TaiKhoan

---
+++ QuanLyCuaHangVatLieuXayDung/service/impl/ITaiKhoanService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuanLyCuaHangVatLieuXayDung.service.impl
+{
+    internal interface ITaiKhoanService
+    {
+        //Lấy danh sách tài khoản 
+        //Lấy tài khoản thông qua mã tài khoản
+        //Tìm kiếm theo Key (username, số điện thoại)
+        //CRUD tài khoản
+        //Kiểm tra điều kiện Login(username, password)
+
+
+
+    }
+}
