ea522e3d79a385ddd7c43323c96d008cd04e2405
HieuCorn364 <hieungo.030604@gmail.com>
2025-04-12 17:00:43 +0700

    Done thay đổi Database cũ là Done PhieuGhiNoService.cs

---
+++ QuanLyCuaHangVatLieuXayDung/service/IPhieuGhiNoService.cs
@@ -0,0 +1,31 @@
+﻿using QuanLyCuaHangVatLieuXayDung.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangVatLieuXayDung.service
+{
+    internal interface IPhieuGhiNoService
+    {
+        List<PhieuGhiNo> findAll();
+
+        PhieuGhiNo findByMaPhieu(string maPhieu);
+
+        bool insertPhieuGhiNo(PhieuGhiNo phieuGhiNo);
+
+        bool deletePhieuGhiNo(string maPhieu);
+
+        List<PhieuGhiNo> searchByKey(string key);
+
+        List<PhieuGhiNo> findByLoaiPhieu(byte loaiPhieu);
+
+        List<PhieuGhiNo> findByDateRange(DateTime startDate, DateTime endDate);
+
+        List<PhieuGhiNo> findByDoiTac(string maDoiTac);
+
+        List<PhieuGhiNo> findByTrangThai(bool trangThai);
+        bool updateTrangThai(string maPhieu, bool trangThai);
+    }
+}
