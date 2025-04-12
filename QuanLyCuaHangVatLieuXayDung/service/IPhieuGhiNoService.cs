d723bb78050b60b72dc9de00e21cfcc32dcec6b0
HieuCorn364 <hieungo.030604@gmail.com>
2025-04-12 17:07:25 +0700

    Done thay đổi Database cũ và Done PhieuGhiNoService.cs

---
+++ QuanLyCuaHangVatLieuXayDung/service/IPhieuGhiNoService.cs
@@ -0,0 +1,32 @@
+﻿using QuanLyCuaHangVatLieuXayDung.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangVatLieuXayDung.service
+{
+    internal interface IPhieuGhiNoService
+    {
+        List<PhieuGhiNo> findAll();
+
+        PhieuGhiNo findByMaPhieu(string maPhieu);
+
+        bool insertPhieuGhiNo(PhieuGhiNo phieuGhiNo);
+
+        bool deletePhieuGhiNo(string maPhieu);
+
+        /// <returns>Danh sách phiếu ghi nợ khớp với từ khóa</returns>
+        List<PhieuGhiNo> searchByKey(string key);
+        List<PhieuGhiNo> findByLoaiPhieu(byte loaiPhieu);
+
+        List<PhieuGhiNo> findByDateRange(DateTime startDate, DateTime endDate);
+
+        List<PhieuGhiNo> findByDoiTac(string maDoiTac);
+
+        List<PhieuGhiNo> findByTrangThai(bool trangThai);
+
+        bool updateTrangThai(string maPhieu, bool trangThai);
+    }
+}
